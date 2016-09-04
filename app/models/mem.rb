8fc08dcb4ff6999b2ed86f23a986233e5ee301da
4rkz <arek.zd@gmail.com>
2016-09-04 15:01:41 +0200

    Fixed old syntax

--- app/models/mem.rb
+++ app/models/mem.rb
@@ -1,11 +1,11 @@
 class Mem < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image,
-                    :styles => {:medium => "300x300>", :thumb => "100x100>"},
-                    :default_url => "images/:style/missing.png"
+                    styles: { medium: "300x300>", thumb: "100x100>"},
+                    default_url: "images/:style/missing.png"
 
   validates_attachment :image,
-                       :content_type => {:content_type => ["image/jpg", "image/gif", "image/png", "image/jpeg"]}
+                       content_type: {content_type: ["image/jpg", "image/gif", "image/png", "image/jpeg"]}
   validates :name, presence: true
 
   after_validation(on: :create) do
